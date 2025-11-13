b72b8d0e9b316381312923dd5f9e8698952f6ffa
Oleksandr Merkulov <odin@terradeus.org>
2025-11-13 05:47:41 +0200

    add src

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,21 @@
 'use strict';
+
+const page = document.querySelector('.page');
+
+function openClose() {
+  const elements = document.querySelectorAll('[data-open-close]');
+
+  elements.forEach((el) => {
+    el.addEventListener('click', () => {
+      const targetName = el.dataset.openClose;
+      const targetElement = document.querySelector(`.${targetName}`);
+
+      if (targetElement) {
+        targetElement.classList.toggle(`${targetName}--open`);
+        page.classList.toggle('page--overflow');
+      }
+    });
+  });
+}
+
+document.addEventListener('DOMContentLoaded', openClose);
