546f180be394d18bf2016275e43d5f2b59008ff7
Oleksandr Merkulov <odin@terradeus.org>
2025-11-22 21:05:33 +0200

    Updates

---
+++ layout_landing-page.591c9fbf.js
@@ -0,0 +1,2 @@
+document.addEventListener("DOMContentLoaded",function(){document.querySelectorAll("[data-open-close]").forEach(e=>{e.addEventListener("click",()=>{let t=e.dataset.openClose,o=document.querySelector(`.${t}`);o&&o.classList.toggle(`${t}--open`)})})});
+//# sourceMappingURL=layout_landing-page.591c9fbf.js.map
